b4c37e42f161dc24b9d0eccffbe02129b01cb543
Thatguyjosh025 <163397123+Thatguyjosh025@users.noreply.github.com>
2024-03-24 15:35:08 +0800

    Delete Authentication.php

--- Authentication.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] !== 'Admin') {
-    // Redirect to login page or any other appropriate action
-    header("Location: Index.php");
-    exit();
-}
-?>
