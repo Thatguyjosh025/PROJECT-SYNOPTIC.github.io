29ab22b25218c33ab188210d71cebf24f46dedcd
Thatguyjosh025 <163397123+Thatguyjosh025@users.noreply.github.com>
2024-04-21 17:32:49 +0800

    Delete System directory

--- System/studentAuthentication.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] !== 'Student') {
-    // Redirect to login page or any other appropriate action
-    header("Location: Index.php");
-    exit();
-}
-
-?>
